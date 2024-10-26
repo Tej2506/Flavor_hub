286ad40323b7eefee3048ec754b4ee355408f810
Basavanna <basavannatej25@gmail.com>
2024-10-26 12:12:36 +0530

    added routes for Login, home, logout and UserProfile

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+//App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Signup from './Signup';
+import Login from './Login';
+import Logout from './Logout'; 
+import Home from './Home';
+import UserProfile from './UserProfile'
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path = "/userprofile/:user_id" component={UserProfile}/>
+        </Switch>
+      </div>
+    </Router>
+  
+   
+  );
 }
 
 export default App;
