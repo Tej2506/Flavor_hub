c7540bc628ee7edb601695cd31a64c230667a94f
Basavanna <basavannatej25@gmail.com>
2024-10-27 14:44:06 +0530

    added functionality to handle fetch requests user/profile, user/dishes

--- server/app.py
+++ server/app.py
@@ -56,20 +56,104 @@ class LogoutResource(Resource):
 
 class CheckLoginResource(Resource):
     def get(self):
-        if session['user_id']:
+        if session.get('user_id'):
             return make_response({'message': 'User is logged in'},200)
 
         else:
             return make_response({'message': 'User is not logged in'}, 401)
+
+class UserProfileResource(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return make_response({'message': 'User not logged in'}, 401)
+
+        user = User.query.get(user_id)
+        if not user:
+            return make_response({'message': 'User not found'}, 404)
+
+        # Calculate the total number of dishes
+        total_dishes = Recipe.query.filter_by(user_id=user_id).count()  # Assuming 'Recipe' is each unique dish instance
+        public_dishes = Recipe.query.filter_by(user_id=user_id, public=True).count()
+
+        user_data = {
+            'username': user.username,
+            'total_dishes': total_dishes,
+            'public_dishes': public_dishes
+        }
+        return make_response(user_data.to_dict(), 200)
+
+class AllDishesRecipes(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+             return make_response({'message': 'User not logged in'}, 401)
+        
+        dishes = Recipe.query.filter_by(user_id = user_id).all()
+        if dishes:
+            all_dishes=[]
+            for dish in dishes:
+                comments_data = []
+                for comment in dish.recipe.comments:
+                    comment_data = {
+                        'username': comment.user.username,  # Access username through the user relationship
+                        'content': comment.content,
+                        'date': comment.date_created
+                    }
+                    comments_data.append(comment_data)
+                dish.to_dict()['comments'] = comments_data 
+
+                all_dishes.append({**dish.to_dict(),'name':dish.dish_name})
+                
+            
+            return make_response(all_dishes,200)
+        
+        return make_response({'message':'No dishes added'},201)
+
 
 # Views go here!
 api.add_resource(SignupResource, '/signup')
 api.add_resource(LoginResource, '/login')
 api.add_resource(LogoutResource, '/logout')
 api.add_resource(CheckLoginResource, '/checklogin')
-
+# api.add_resource(DishDetailResource, '/dishdetail/<int:dish_id>')
+api.add_resource(UserProfileResource, '/userprofile')
+api.add_resource(AllDishesRecipes, '/user/dishes')
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
+class DishDetailResource(Resource):
+#     def get(self, dish_id):
+#         dish = Dish.query.get(dish_id)
+#         if not dish:
+#             return make_response({'message': 'Dish not found'}, 404)
+
+#         # Prepare dish data
+#         dish_data = {
+#             'id': dish.id,
+#             'name': dish.name,
+#             'description': dish.recipe.description,
+#             'image_url': dish.recipe.image_url,
+#             'date_created': dish.recipe.date_created,
+#             'ingredients': [recipe.ingredients for recipe in dish.recipes],
+#             'instructions': [recipe.instructions for recipe in dish.recipes],
+#             'tags': dish.tag_names
+#         }
+
+#         # Get comments with usernames
+#         comments_data = []
+#         for recipe in dish.recipes:
+#             for comment in recipe.comments:
+#                 comment_data = {
+#                     'username': comment.user.username,  # Access username through the user relationship
+#                     'content': comment.content,
+#                     'date': comment.date_created
+#                 }
+#                 comments_data.append(comment_data)
+
+#         # Add comments to dish data
+#         dish_data['comments'] = comments_data
+
+#         return make_response(dish_data.to_dict(), 200)
